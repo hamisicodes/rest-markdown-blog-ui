689306243722d071375fa76345ebe24522910100
hamisicodes <newhamisi@gmail.com>
2020-10-14 07:45:41 +0300

    use a custom hook to refactorrepetitive code

--- src/containers/PostList.js
+++ src/containers/PostList.js
@@ -1,35 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {NavLink} from "react-router-dom";
 import { Divider , Header, Item } from "semantic-ui-react";
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import {api} from '../api'
-
+import {api} from '../api';
+import { useFetch } from '../helpers';
 
 
 function PostList() {
-  const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      setLoading(true);
-
-      try {
-        const res = await axios.get(api.posts.list);
-
-        setPosts(res.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    }
-
-    fetchData();
-  }, []);
+  
+  const {data,loading,error} = useFetch(api.posts.list)
 
   return (
     <div>
@@ -38,7 +18,7 @@ function PostList() {
       {error && <Message negative message={error} />}
       {loading && <Loader/>}
       <Item.Group>
-          { posts?.map(post => {
+          { data?.map(post => {
               return(
                 <Item key={post.id}>
                 <Item.Image size="small" src={post.thumbnail} />
